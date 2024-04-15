fbb98b2077da1b302db3acbf19ccd9be5ad8a489
oumaima <o.khajaa@sophatel.com>
2024-04-15 15:26:27 +0100

    chore: update folders structure

--- Winmed-Angular/src/app/app.module.ts
+++ Winmed-Angular/src/app/app.module.ts
@@ -3,16 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { NavbarComponent } from './navbar/navbar.component';
-import { HeroSectionComponent } from './hero-section/hero-section.component';
-import { FunctionalitiesSectionComponent } from './functionalities-section/functionalities-section.component';
-import { PerformanceSectionComponent } from './performance-section/performance-section.component';
-import { PricingSectionComponent } from './pricing-section/pricing-section.component';
-import { TestimonialSectionComponent } from './testimonial-section/testimonial-section.component';
-import { CallToActionSectionComponent } from './call-to-action-section/call-to-action-section.component';
-import { FooterComponent } from './footer/footer.component';
-import { PricingCardComponent } from './pricing-card/pricing-card.component';
-import { TextBoxComponent } from './text-box/text-box.component';
+import { NavbarComponent } from './landing-page/components/navbar/navbar.component';
+import { HeroSectionComponent } from './landing-page/components/hero-section/hero-section.component';
+import { FunctionalitiesSectionComponent } from './landing-page/components/functionalities-section/functionalities-section.component';
+import { PerformanceSectionComponent } from './landing-page/components/performance-section/performance-section.component';
+import { PricingSectionComponent } from './landing-page/components/pricing-section/pricing-section.component';
+import { TestimonialSectionComponent } from './landing-page/components/testimonial-section/testimonial-section.component';
+import { CallToActionSectionComponent } from './landing-page/components/call-to-action-section/call-to-action-section.component';
+import { FooterComponent } from './landing-page/components/footer/footer.component';
+import { PricingCardComponent } from './shared/components/pricing-card/pricing-card.component';
+import { TextBoxComponent } from './shared/components/text-box/text-box.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { TextBoxComponent } from './text-box/text-box.component';
     CallToActionSectionComponent,
     FooterComponent,
     PricingCardComponent,
-    TextBoxComponent
+    TextBoxComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
